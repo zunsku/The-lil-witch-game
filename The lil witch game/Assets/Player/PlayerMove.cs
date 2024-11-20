987401a542f98ca2960a0035eec1ed8a1f9ba580
zunsku <112386243+zunsku@users.noreply.github.com>
2024-11-20 03:02:16 +0200

    Added player movement and camera controls(Redo)
    Added player movement and camera controls. Alongside for the camera controls, the Cinemachine addon was added. This process was redone, since it was accidentally done in reverse the first time.

---
+++ The lil witch game/Assets/Player/PlayerMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+public Rigidbody rb;
+    public float speed;
+    public float rotationSpeedHorizontal;
+    public float rotationSpeedVertical;
+    private float vertical;
+    private float horizontal;
+    private float horizontalLook;
+    private float verticalLook;
+    public CinemachineVirtualCamera VirtualCamera;
+    public GameObject Player;
+    public KeyCode Restart;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        //cam = Camera.main;
+        VirtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = 1f;
+    }
+
+    private void FixedUpdate()
+    {
+        
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        rb.velocity = (transform.right * horizontal + transform.forward * vertical) * speed * Time.deltaTime;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Restart))
+        {
+            Player.transform.position = new Vector3(0, 1, 0);
+        }
+        
+        horizontalLook = Input.GetAxis("Mouse X");
+        verticalLook += Input.GetAxis("Mouse Y") * rotationSpeedVertical;
+
+        transform.Rotate((transform.up * horizontalLook) * rotationSpeedHorizontal * Time.deltaTime);
+        VirtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = (verticalLook);
+    }
+}
